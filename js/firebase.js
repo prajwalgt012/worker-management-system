e5db2f81ea3288b1e22f66763c307af9ce576565
prajwalgt012 <157192961+prajwalgt012@users.noreply.github.com>
2025-07-03 16:18:23 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,14 @@
+// Initialize Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-project-id",
+  apiKey: "AIzaSyC3aue5Fg1Rpqk3rzkjQiQTBe76U6RA7GM",
+  authDomain: "worker-management-50d6d.firebaseapp.com",
+  projectId: "worker-management-50d6d",
+  storageBucket: "worker-management-50d6d.firebasestorage.app",
+  messagingSenderId: "981353658232",
+  appId: "1:981353658232:web:d6869633271f3309133a8d",
+  measurementId: "G-62MLYTQ6WJ"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-
