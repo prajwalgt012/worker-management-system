0543ac92b6b96c5e92604250878451fd795f3cc9
prajwalgt012 <157192961+prajwalgt012@users.noreply.github.com>
2025-07-03 16:15:53 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-project-id",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 
