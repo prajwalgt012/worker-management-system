06bc680aeb6a54bd237b297d14769201a0b46d50
prajwalgt012 <157192961+prajwalgt012@users.noreply.github.com>
2025-07-03 16:15:17 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(user => {
+      window.location.href = "manager-dashboard.html";
+    })
+    .catch(error => alert(error.message));
+}
 
